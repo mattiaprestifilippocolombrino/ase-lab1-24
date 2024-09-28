84a0d7eed65e2f39f0c3ce7d60f8629dac8c8964
mattiaprestifilippocolombrino <m.prestifilippoco@studenti.unipi.it>
2024-09-28 13:25:18 +0200

    28 sept final commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__, instance_relative_config=True)
@@ -12,14 +13,41 @@ def add():
         return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
 
 #Endpoint /sub for subtraction which takes a and b as query parameters.
-
+@app.route('/sub')
+def sub():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a-b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
 #Endpoint /mul for multiplication which takes a and b as query parameters.
-
+@app.route('/mul')
+def mul():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a*b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
 #Endpoint /div for division which takes a and b as query parameters. Returns HTTP 400 BAD REQUEST also for division by zero.
-
+@app.route('/div')
+def div():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a/b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
 #Endpoint /mod for modulo which takes a and b as query parameters. Returns HTTP 400 BAD REQUEST also for division by zero.
-
-#Endpoint /random which takes a and b as query parameters and returns a random number between a and b included. Returns HTTP 400 BAD REQUEST if a is greater than b.
+@app.route('/mod')
+def mod():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a%b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
 
 if __name__ == '__main__':
     app.run(debug=True)
